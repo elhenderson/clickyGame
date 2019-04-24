c99ae635ee4aaea1d1968a9e08c27094b9f8be69
Evann Henderson <ehenderson@sku2u.com>
2019-04-24 16:01:18 -0500

    count button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from "./components/Header"
 
-App = () => {
+function App() {
   return <Header />
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-Header = (props) => {
+function Header(props) {
+  const [count, setCount] = useState(0)
+
   return(
     <div id="header">
-      Clicky Game
+      <p>Score: {count}</p>
+      <p>Top Score: {count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me!
+      </button>
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
+
     </div>
   )
 }
